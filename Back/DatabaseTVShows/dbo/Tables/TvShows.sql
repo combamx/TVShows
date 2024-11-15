9ca101e97b14e414b26c7a7a4c15b4c2b9721d8f
Omar Cortes <omar.cortes.casillas@gmail.com>
2024-11-15 10:01:25 -0600

    Generar DB

---
+++ Back/DatabaseTVShows/dbo/Tables/TvShows.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[TvShows] (
+    [Id]             INT            IDENTITY (1, 1) NOT NULL,
+    [Name]           NVARCHAR (255) NOT NULL,
+    [Favorite]       BIT            NOT NULL,
+    [Content]        NVARCHAR (255) NOT NULL,
+    [Format]         NVARCHAR (255) NOT NULL,
+    [Episodes]       NVARCHAR (255) NOT NULL,
+    [Duration]       NVARCHAR (50)  NOT NULL,
+    [Scenarios]      NVARCHAR (255) NOT NULL,
+    [Classification] NVARCHAR (50)  NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ Back/DatabaseTVShows/dbo/Tables/Users.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]       INT            IDENTITY (1, 1) NOT NULL,
+    [Name]     NVARCHAR (255) NOT NULL,
+    [Email]    NVARCHAR (255) NOT NULL,
+    [Password] NVARCHAR (255) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
